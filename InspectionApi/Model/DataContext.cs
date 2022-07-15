d243ec4e573f8d50c0c249278ccc6baede193011
jose moncada <andresmonk2010@hotmail.com>
2022-07-14 20:52:47 -0500

    Agregar archivos de proyecto.

---
+++ InspectionApi/Model/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InspectionApi.Model
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Status>Statuses { get; set; }
+
+    }
+}
---
+++ InspectionApi/Model/InspectionType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionApi.Model
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public String InsepctionName { get; set; } = String.Empty;
+    }
+}
